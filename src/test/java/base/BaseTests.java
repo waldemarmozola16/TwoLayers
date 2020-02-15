185e4848994cb9b160fd2912bb5023ea016dcb45
Kali <you@example.com>
2020-02-15 10:17:25 +0100

    create testSuccessfulLogin

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,21 +1,25 @@
 package base;
 
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+
+import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        driver.manage().window().setSize(new Dimension(375, 812));
+        homePage = new HomePage(driver);
 
-        System.out.println(driver.getTitle());
         driver.quit();
     }
 
---
+++ src/test/java/login/LoginTests.java
@@ -0,0 +1,9 @@
+package login;
+
+public class LoginTests {
+
+    public void testSuccessfulLogin() {
+
+    }
+
+}
