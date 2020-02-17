26a20a4d0268fa140914dd7fbca4869e33ac4c8c
Kali <you@example.com>
2020-02-17 15:00:55 +0100

    Create loginTests

--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -1,9 +1,15 @@
 package login;
 
-public class LoginTests {
+import base.BaseTests;
+import pages.LoginPage;
+
+public class LoginTests extends BaseTests {
 
     public void testSuccessfulLogin() {
-
+        LoginPage loginPage = homePage.clickFormAuthentication();
+        loginPage.setUsername("tomsmith");
+        loginPage.setPassword("SuperSecretPassword!");
+        loginPage.clickLoginButton();
     }
 
 }
