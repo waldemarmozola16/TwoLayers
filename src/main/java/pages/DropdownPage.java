aa62abb13dd384f57460aba14019c5d15b7834fb
Kali <you@example.com>
2020-03-20 15:03:47 +0100

    Add method addMultipleAttribute

--- src/main/java/pages/DropdownPage.java
+++ src/main/java/pages/DropdownPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
@@ -25,6 +26,11 @@ public class DropdownPage {
         List<WebElement> selectedElements =
                 findDropDownElement().getAllSelectedOptions();
         return selectedElements.stream().map(e->e.getText()).collect(Collectors.toList());
+    }
+
+    public void addMultipleAttribute(){
+        String script = "arguments[0].setAttribute('multiple', '')";
+        ((JavascriptExecutor)driver).executeScript(script, findDropDownElement());
     }
 
     private Select findDropDownElement(){
